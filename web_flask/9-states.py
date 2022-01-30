8a235a401470b839e7173f2d44cd9c0c571b10ad
Daniel-bee <daniel123_86@live.com>
2022-01-30 16:19:29 +0300

    state id

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -8,6 +8,15 @@ from flask import Flask, render_template
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
+@app.route('/states')
+def states():
+    """
+        Retern state object
+        from route /states_list
+    """
+    states = storage.all("State")
+    return render_template('9-states.html', state_list=states)
+
 
 @app.route('/states')
 @app.route('/states/<id>')
